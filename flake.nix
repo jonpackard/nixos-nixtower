39213dc85951d8fc7d8f0144c512e9ffd094b808
Jonathan Packard <jwpackard@duck.com>
2025-03-03 23:20:04 -0500

    Nixpkgs now follows nixpkgs from Jovian-NixOS

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
-  description = "Your new nix config";
+  description = "NixOS on Steam Deck with SteamOS GUI";
 
   inputs = {
+    # Jovian-NixOS - customizations for Steam Deck
+    jovian-nixos.url = "github:Jovian-Experiments/Jovian-NixOS";
+
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/64e75cd44acf21c7933d61d7721e812eac1b5a0a"; #Using the same nixpkgs-unstable commit in flake.lock here https://github.com/Jovian-Experiments/Jovian-NixOS/blob/development/flake.lock
+    nixpkgs.follows = "jovian-nixos/nixpkgs"; #Use the nixpkgs defined by jovian-nixos flake.
+
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -12,10 +16,6 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Jovian-NixOS - customizations for Steam Deck
-    jovian-nixos.url = "github:Jovian-Experiments/Jovian-NixOS";
-    jovian-nixos.inputs.nixpkgs.follows = "nixpkgs";
 
   };
 
