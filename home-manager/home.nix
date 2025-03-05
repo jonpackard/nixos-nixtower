65ecaba52e9ef2ab31398a9b4207c82d82b691d9
Jonathan Packard <jwpackard@duck.com>
2025-03-05 01:07:53 +0000

    Add fortune package to home-manager config (testing)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -53,7 +53,9 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    fortune
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
